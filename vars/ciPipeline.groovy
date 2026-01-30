7e8665bf56694916962950e2ec4b1ff052a70c7e
Amjad-07 <146514436+Amjad-07@users.noreply.github.com>
2026-01-30 14:53:37 +0530

    Update ciPipeline.groovy

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,19 +1,7 @@
-def call() {
-    pipeline {
-        agent any
-
-        stages {
-            stage('Init') {
-                steps {
-                    echo "Initializing pipeline from shared library"
-                }
-            }
-
-            stage('Build') {
-                steps {
-                    echo "Build stage executed via ciPipeline"
-                }
-            }
+stage('Build') {
+    steps {
+        script {
+            buildApp(config)
         }
     }
 }
