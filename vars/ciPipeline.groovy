ede9aa9c14bd887e4a64a67a85f6675e6c8204d9
Amjad-07 <146514436+Amjad-07@users.noreply.github.com>
2026-01-30 14:59:03 +0530

    Update ciPipeline.groovy

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,7 +1,30 @@
-stage('Build') {
-    steps {
-        script {
-            buildApp(config)
+def call(Closure body) {
+
+    def config = [:]
+
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipeline {
+        agent any
+
+        stages {
+            stage('Init') {
+                steps {
+                    echo "App Name: ${config.appName}"
+                    echo "App Type: ${config.appType}"
+                    echo "Envs    : ${config.environments}"
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    script {
+                        buildApp(config)
+                    }
+                }
+            }
         }
     }
 }
